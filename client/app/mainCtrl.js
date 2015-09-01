bc959131bd96c9baa2e4ee34feacad0a4268ca5b
Evan Mencke <metaevan@gmail.com>
2015-09-01 12:16:22 -0700

    [test] adding address validation

--- client/app/mainCtrl.js
+++ client/app/mainCtrl.js
@@ -8,14 +8,34 @@ angular.module('app',['app.services'])
 
   main.searchInfo = { };
 
+  main.initMap = function() {
+
+  }
+
   main.submitAddress = function() {
     console.log('mainCtrl.js says: Submitted address:', main.address);
-    
-    main.searchInfo.address = main.address;
+
     main.searchInfo.bedrooms = main.bedrooms;
     main.searchInfo.bathrooms = main.bathrooms;
 
-    Api.submit(main.searchInfo);
+    geocoder = new google.maps.Geocoder();
+    geocoder.geocode({ 'address': main.address }, function(results, status) {
+      if (status == google.maps.GeocoderStatus.OK) {
+        console.log(results[0].geometry.location);
+        console.log('results address', results[0].formatted_address);
+        if (!isNaN(results[0].formatted_address[0])) {
+          main.searchInfo.address = results[0].formatted_address;
+          Api.submit(main.searchInfo);
+        } else {
+          alert("Please insert a valid U.S. address");
+        }
+      } else {
+        console.log('error: ', status, ' ', results)
+      }
+    });
+
+
   };
+
 
 }]);
